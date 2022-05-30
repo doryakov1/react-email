4930cab5497ee6fa248b7d7faac81b31d51ae5b4
dor yakov <betshean@walla.com>
2022-05-31 00:04:42 +0300

    first commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 export default function Header(props) {
   return (
     <div className='header'>
     <input onChange={(e)=>{props.filterEmails(e.target.value)}} type='search' placeholder='Search'/>
+    <Link className='link' to={"/react-email"}><h4>Email app</h4></Link>
     </div>
   )
 }
